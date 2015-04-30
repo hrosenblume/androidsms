d513c88242309d288ba4e632eaff4b0f6fc1a64c
hrosenblume <hrosenblume@gmail.com>
2015-04-30 15:16:35 -0400

    made some changes. testing stuff modeled

--- test/test.js
+++ test/test.js
@@ -1,16 +1,29 @@
 var q = require('q');
 var should = require('chai').should();
 var androidSMS = require('android-sms-node');
+var assert = require("assert");
 var createContact = q.denodeify(androidSMS.createContact);
 
-describe('Testing createContact', function() {
-	var name = 'Test Contact';
-	var number = "+5162444385";
+var sample_contact = {
+	name: 'Test Contact',
+	number: '+94812394832',
+	message: 'This is a sample message.'
+};
 
-	it('properly calls the api', function() {
-		createContact(name, number).then(console.log, console.error);
-	});
-	it('creates a contact named ' + name + ' with number ' + number, function() {
-		//help
+describe('Testing createContact', function() {
+	this.timeout(3000);
+
+	it('properly posts to the api', function(done) {
+		androidSMS.createContact(sample_contact.name, sample_contact.number, function(val, err) {
+			if (err) throw(err);
+			var result = JSON.parse(val[1]);
+		  	try {
+		  		assert.notEqual(result, null);
+		  		done();
+		  	} catch(e) {
+		  		console.log(result.errors); 
+		  		done(e); 
+		  	}
+		});
 	});
 });
